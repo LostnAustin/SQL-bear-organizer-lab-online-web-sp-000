180400204c1d006e643e9ac8803c0f6b0001cf2e
Chad Lengefeld <chadlengefeld@gmail.com>
2020-02-13 17:56:10 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-  VALUES ("Mr. Chocolate", 2 , "M", "Brown", "Friendly", 1 );
+  VALUES ("Mr. Chocolate", 2 , "M", "dark brown", "Friendly", 1 );
 
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-  VALUES ("Rowdy", 1, "M", "Brown", "Intense", 1 );
+  VALUES ("Rowdy", 1, "M", "dark brown", "Intense", 1 );
 
 INSERT INTO bears (name, age, gender, color, temperament, alive)
   VALUES ("Tabitha", 6, "F", "Black", "Playful", 0);
@@ -11,10 +11,10 @@ INSERT INTO bears (name, age, gender, color, temperament, alive)
   VALUES ("Sergeant Brown", 5, "M", "Brown", "Wise", 1);
 
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-  VALUES ("Melissa", 13, "F", "Brown", "Friendly", 1 );
+  VALUES ("Melissa", 13, "F", "dark brown", "Friendly", 1 );
 
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-  VALUES ("Grinch", 6, "M", "Brown", "Friendly", 0 );
+  VALUES ("Grinch", 6, "M", "dark brown", "Friendly", 0 );
 
 INSERT INTO bears (name, age, gender, color, temperament, alive)
   VALUES ("Wendy", 6, "F", "Brown", "Friendly", 0 );
